d7c63310af864a02673fec59b2a85085f4693523
Mariia Nedilko <mariia.nedilko@gmail.com>
2023-02-09 22:17:44 +0100

    github images links fixed

--- src/COMPONENTS/MainBanner/MainBanner.jsx
+++ src/COMPONENTS/MainBanner/MainBanner.jsx
@@ -1,10 +1,11 @@
 import {Link} from "react-router-dom";
+import mainBannerbackground from './gym-equipment-react/IMAGES/mainBannerbackground.svg';
 
 
 const MainBanner = () => {
 
     const backgroundImageStyle = {
-        backgroundImage: `url(/IMAGES/mainBannerbackground.svg)`,
+        backgroundImage: `url(${mainBannerbackground})`,
         backgroundPosition: 'center',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
@@ -16,7 +17,7 @@ const MainBanner = () => {
         <>
             <div className="mainBanner mainBanner-desktop">
                 <div className="mainBanner-background" style={backgroundImageStyle}>
-                    <img className="mainBanner-image" src={'/IMAGES/man.svg'}/>
+                    <img className="mainBanner-image" src={'./gym-equipment-react/IMAGES/man.svg'}/>
                     <div className="mainBanner-content">
                         <h1 className=" mainBanner-text mainBanner-text--regular">
                             become a member of
